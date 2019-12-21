39f192ca5d5210252a5d71dcfca6bb7bb62ab15c
AlexOg37 <alexog37@gmail.com>
2019-12-21 16:12:47 +0000

    Deploying to gh-pages from build-setup-2 30f4108a6c897a16ab8a26db081de5d2e1eac5e4

---
+++ precache-manifest.1828bf68336b4bdfceca23b719a90264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b5131d86c162b6089b91034b6cbc3d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd07e835273450d2c709",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73178a8957632e8ebe46",
+    "url": "./static/js/2.6ce4512e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6ce4512e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd07e835273450d2c709",
+    "url": "./static/js/main.47c0a526.chunk.js"
+  },
+  {
+    "revision": "73e8b4d3219d27debbee",
+    "url": "./static/js/runtime-main.59e07fb9.js"
+  }
+]);
