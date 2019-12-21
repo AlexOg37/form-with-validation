114161ae9fec903e197add4aee33974add4b59d8
AlexOg37 <oogorodnyk@atlassian.com>
2019-12-21 09:05:04 +0200

    add actions

---
+++ src/actions.ts
@@ -0,0 +1,34 @@
+export type Values = {
+  name: string;
+  surname: string;
+}
+
+export type Errors = {
+  [key in keyof Values]?: string;
+};
+
+export type State = {
+  values: Values;
+  errors: Errors;
+}
+
+type SetValueAction = {
+  type: 'SET-VALUE',
+  payload: { value: Values[keyof Values], fieldName: keyof Values }
+}
+export const setValue = <T extends keyof Values>(fieldName: T, value: Values[T]): SetValueAction => ({
+  type: 'SET-VALUE',
+  payload: { fieldName, value }
+});
+
+type SetErrorAction = {
+  type: 'SET-ERROR',
+  payload: { error: string, fieldName: keyof Values }
+}
+export const setError = <T extends keyof Values>(fieldName: T, error: Values[T]): SetErrorAction => ({
+  type: 'SET-ERROR',
+  payload: { fieldName, error }
+});
+
+
+export type Actions = SetErrorAction | SetValueAction;
