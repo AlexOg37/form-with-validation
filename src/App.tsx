53ff07459e23da69b4c3349ee33ddb3b84ee62d3
AlexOg37 <oogorodnyk@atlassian.com>
2019-12-21 09:06:30 +0200

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
+import { TextInput } from './TextInput';
+import { formReducer, defaultState } from './reducer';
+import { setValue, Values, setError } from './actions';
+import { validateRequired, validateMinMax } from './validation';
+
+const validateName = (value: string): string | undefined => {
+  return validateRequired(value) || validateMinMax(5, 10)(value)
+}
 
 const App: React.FC = () => {
+  const [state, dispatch] = useReducer(formReducer, defaultState);
+  const { values, errors } = state;
+
+  const handleTextFiledChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    dispatch(setValue(name as keyof Values, value));
+  }
+
+  const handleTextFiledTouch = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    const error = validateName(value || '') || '';
+    dispatch(setError(name as keyof Values, error));
+  }
+  
+  const isFormValid = () => {
+    const noErrors = (Object.keys(values) as Array<keyof typeof values>)
+      .every(k => errors[k] === '');
+    return noErrors;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TextInput
+        label='First name'
+        name='name'
+        value={values.name}
+        onChange={handleTextFiledChange}
+        onBlur={handleTextFiledTouch}
+        error={errors.name}
+      />
+      <br/>
+      <TextInput
+        label='Last name'
+        name='surname'
+        value={values.surname}
+        onChange={handleTextFiledChange}
+        onBlur={handleTextFiledTouch}
+        error={errors.surname}
+      />
+      <br/>
+      <button type='submit' disabled={!isFormValid()}>Submit</button>
+    </>
   );
 }
 
